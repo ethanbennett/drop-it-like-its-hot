a1dac95c3df364f8b2b0e71d34fb1eb57df42249
David Knott <david1k1nott@gmail.com>
2017-02-08 12:50:36 -0700

    delete user table

--- db/migrate/20170208193033_remove_date_columns.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDateColumns < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :create_at
-    remove_column :users, :updated_at
-  end
-end
--- db/migrate/20170208193248_add_user_timestamps.rb
+++
@@ -1,5 +0,0 @@
-class AddUserTimestamps < ActiveRecord::Migration[5.0]
-  def change
-    add_timestamps(:users)
-  end
-end
