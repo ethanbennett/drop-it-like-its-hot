d8d840b60bf706588f8f84c68e377ab3b818a83b
David Knott <david1k1nott@gmail.com>
2017-02-08 21:44:27 -0700

    google authentication redirect works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,18 @@ class User < ApplicationRecord
   has_many :repos
 
   has_secure_password
-  validates :first_name, :last_name, :email, :role, presence: true
+  validates :first_name, :last_name, :email, password, :role, presence: true
 
   enum role: [ :user, :admin ]
 
   attr_accessor   :agrees
 
+  def self.from_omniauth(auth))
+    user = User.find_by_or_create(email: auth["info"]["email"])
+    user.first_name = auth["info"]["name"].split(" ").first
+    user.last_name  = auth["info"]["name"].split(" ").last
+    user.email      = auth["info"]["email"]
+    user.role       = "user"
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root "landing#show"
   get   "/login", to: "sessions#new"
-  post  "/logins", to: "sessions#create"
+  get   "/auth/google_oauth2/callback", to: "sessions#create"
+  # post   "/auth/google_oauth2/callback", to: "sessions#create"
+  get   "/auth/google_oauth2/callback", to: "sessions#new"
+  post  "/login", to: "sessions#create"
+
   resources :users, only: [:new,:create]
   resources :home, only: [:index]
+
 end
