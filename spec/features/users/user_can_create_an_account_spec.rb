b68073357be0d470a4b28294b22a2354e3292208
David Knott <david1k1nott@gmail.com>
2017-02-09 15:56:50 -0700

    create flash partial

--- spec/features/users/user_can_create_an_account_spec.rb
+++ spec/features/users/user_can_create_an_account_spec.rb
@@ -34,6 +34,22 @@ describe "When a user visits the landing page" do
     click_on "Create an account"
 
     expect(current_path).to eq users_path
-    expect(page).to have_content "asdas"
+    expect(page).to have_content "Password can't be blank"
+  end
+
+  scenario "they need to check the agree box" do
+    visit root_path
+
+    click_on "Create Account"
+
+    fill_in :user_first_name, :with => "Bob"
+    fill_in :user_last_name, :with => "Dylan"
+    fill_in :user_email, :with => "bob@gmail.com"
+    fill_in :user_password, :with => "1234"
+
+    click_on "Create an account"
+
+    expect(current_path).to eq users_path
+    expect(page).to have_content "Password can't be blank"
   end
 end
