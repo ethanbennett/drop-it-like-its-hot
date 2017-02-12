7eb2c6bede6bef7b30a5b5ae974a384a79e7cedf
Ethan Bennett <ebennett19@gmail.com>
2017-02-12 13:18:02 -0700

    Add factories to test document and folder views

--- spec/factories/folders.rb
+++ spec/factories/folders.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
   factory :folder do
     name "Folder"
+
+    factory :folder_with_documents do
+      after(:create) do |folder|
+        create_list(:document, 2, repo: folder)
+      end
+    end 
+
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,14 @@ FactoryGirl.define do
     image "link"
     password "password"
     role 0
+
+    factory :user_with_repos do
+      after(:create) do |user|
+        create(:folder_with_documents, user: user)
+      end
+    end
   end
+  
   factory :admin, class: User do
     first_name "Firstname"
     last_name "Lastname"
