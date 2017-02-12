cc6abaf9aa96ab5752014365bceede18414638b8
David Knott <david1k1nott@gmail.com>
2017-02-11 19:30:05 -0700

    fullscreen drag and drop to aws working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -83,19 +83,3 @@ $(document).ready(function () {
         }
     });
 });
-
-$(document).ready(function () {
-    Dropzone.autoDiscover = false;
-    $("#dZUpload").dropzone({
-        url: "hn_SimpeFileUploader.ashx",
-        addRemoveLinks: true,
-        success: function (file, response) {
-            var imgName = response;
-            file.previewElement.classList.add("dz-success");
-            console.log("Successfully uploaded :" + imgName);
-        },
-        error: function (file, response) {
-            file.previewElement.classList.add("dz-error");
-        }
-    });
-});
