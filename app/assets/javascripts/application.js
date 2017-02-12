2cdc1cbe2bcac5bd5c91047662b206b8f39da6a8
David Knott <david1k1nott@gmail.com>
2017-02-12 11:02:13 -0700

    remove dropzone and add in repo controller from ethans branch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -70,16 +70,17 @@ $(function() {
 
 $(document).ready(function () {
     Dropzone.autoDiscover = false;
-    $("#dZUpload").dropzone({
-        url: "hn_SimpeFileUploader.ashx",
+    $(".drop").dropzone({
+        url: $(".searchResults").data("query"),
         addRemoveLinks: true,
         success: function (file, response) {
             var imgName = response;
             file.previewElement.classList.add("dz-success");
             console.log("Successfully uploaded :" + imgName);
+            $.get(root_path);
         },
         error: function (file, response) {
             file.previewElement.classList.add("dz-error");
         }
     });
-});
+})
