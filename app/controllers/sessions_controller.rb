01b94b6bfba347fa21da354ecb4587c7932c2481
Ethan Bennett <ebennett19@gmail.com>
2017-02-11 18:14:55 -0700

    Add routes for repos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to home_index_path
+      redirect_to user_repos_path(user.id)
     else
       flash[:danger] = "Invalid email/password combination"
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "landing#show"
   get   "/login", to: "sessions#new"
   post  "/logins", to: "sessions#create"
-  resources :users, only: [:new,:create]
+  resources :users, only: [:new,:create] do
+    resources :repos, only: [:index, :show]
+  end
   resources :home, only: [:index]
   resources :account, only: [:index]
   get "/aws-test", to: "awstest#index"
