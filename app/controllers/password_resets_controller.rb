1aed7f1d3a23b874c52a73a26d0bd038f37af922
David Knott <david1k1nott@gmail.com>
2017-02-15 15:36:10 -0700

    create password reset sad path

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,6 +9,10 @@ class PasswordResetsController < ApplicationController
     verification_code = "123"
     session[:verification_code] = verification_code
     user = User.find_by(email: params[:email])
+    unless user
+      flash[:danger] = "Please enter a valid email"
+      return redirect_to new_password_reset_path
+    end
     session[:forgot_password_user_id] = user.id
     # MessageSender.send_code(user.phone_numbwer, verification_code)
     redirect_to new_phone_verification_path
