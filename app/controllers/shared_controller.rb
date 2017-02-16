9e9c2b83fefa75c750be43c344e05d97d513ce11
Ethan Bennett <ebennett19@gmail.com>
2017-02-16 02:18:59 -0700

    Password check is functional in downloads index

--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -1,7 +1,7 @@
 class SharedController < ApplicationController
   def show
     @repo = Repo.find(params[:id])
-    if repo.password.nil? || password_check
+    if @repo.password.nil? || password_check
       send_data Download.from_s3(@repo), :filename => @repo.name
     end
   end
