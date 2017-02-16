07f847fcad7665d3bacc8c3d839e7f89ac8c0344
Ethan Bennett <ebennett19@gmail.com>
2017-02-16 00:33:00 -0700

    Add optional password field to modal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,16 @@
 class HomeController < ApplicationController
 
-  # def show
-  #   @user_dashboard = UserDashboard.new(user: current_user)
-  #   @repo = Repo.new
-  #   @repos = current_user.repos.where(repo_id: current_repo)
-  # end
+  def show
+    @user_dashboard = UserDashboard.new(user: current_user)
+    @repo = Repo.new
+    @repos = current_user.repos.where(repo_id: current_repo)
+  end
 
   def index
     @user_dashboard = UserDashboard.new(user: current_user)
     @repo = Repo.new
     @repos = Repo.where(repo_id: nil)
-    @repos = current_repo.repos if current_repo
+    # @repos = current_repo.repos if current_repo
     @s3_direct_post = Upload.to_s3
   end
 end
