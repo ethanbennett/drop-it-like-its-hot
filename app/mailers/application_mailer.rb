581faff2d7453268a5cef95ec9f3c935c6070b26
Ethan Bennett <ebennett19@gmail.com>
2017-02-15 20:18:30 -0700

    Implement mailer action

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'ebennett19@gmail.com'
   layout 'mailer'
 end
--- app/mailers/sharing_mailer.rb
+++ app/mailers/sharing_mailer.rb
@@ -1,2 +1,6 @@
 class SharingMailer < ApplicationMailer
+  def sharing_email(email)
+    @user = User.find(session[:user_id])
+    mail(to: email, subject: "#{@user.first_name} wants to share a document with you!")
+  end
 end
