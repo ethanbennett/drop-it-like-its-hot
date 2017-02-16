5e1300f40d9e1db04e4dc05399a0cde757ca9619
Ethan Bennett <ebennett19@gmail.com>
2017-02-15 18:59:55 -0700

    Back end of password protection should be working

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,7 +2,15 @@ class DownloadsController < ApplicationController
   def show
     #.available_repos
     repo = current_user.repos.find(params[:id])
-    #instead of test.jpg we will use repo.name
-    send_data Download.from_s3(repo), :filename => "test.jpg"
+    if repo.password.nil? || password_check
+      #instead of test.jpg we will use repo.name
+      send_data Download.from_s3(repo), :filename => "test.jpg"
+    end
+  end
+
+private
+
+  def password_check
+    repo.password && repo.authenticate(params[:password])
   end
 end
