dcfdacdca4a967ae66624d9b93cd10a02ef8d24f
David Knott <david1k1nott@gmail.com>
2017-02-15 21:06:45 -0700

    create download controller test

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,5 @@
 class DownloadsController < ApplicationController
   def show
-    #.available_repos
     repo = current_user.repos.find(params[:id])
     if repo.password.nil? || password_check
       send_data Download.from_s3(repo), :filename => repo.name
