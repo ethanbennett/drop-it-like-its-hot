cc6d031e65a5bc79e6319c96babaebc643acb0cd
Ethan Bennett <ebennett19@gmail.com>
2017-02-15 20:23:07 -0700

    Add mailer view

--- app/mailers/sharing_mailer.rb
+++ app/mailers/sharing_mailer.rb
@@ -1,6 +1,8 @@
 class SharingMailer < ApplicationMailer
   def sharing_email(email)
     @user = User.find(session[:user_id])
+    # Either pass in email from wherever this method is called or
+    # look into accessing params in mailer
     mail(to: email, subject: "#{@user.first_name} wants to share a document with you!")
   end
 end
